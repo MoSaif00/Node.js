863437bb950fa594e13f0ff1640f308c4de1d3c2
MoSaif00 <moh14ab@gmail.com>
2021-02-08 00:21:04 +0100

    added html files in view folder

--- week1/homework/hackyourtemperature/server.js
+++ week1/homework/hackyourtemperature/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 const app = express();
+app.use(express.json());
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({defaultLayout: false}));
 
 app.get('/', (req, res) => {
-  res.send('<h1>hello from backend to frontend!!!</h1>');
+  res.render(index);
 });
 
+// Listen to local host on port 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server initialized successfully at ${port}`);
