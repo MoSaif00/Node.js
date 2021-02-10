97c5cf3db10f66859cbc9123974c71522bbcb2ff
MoSaif00 <moh14ab@gmail.com>
2021-02-10 15:07:25 +0100

    updated the project by adding error handling and also fixed the read all files title by adding folder for all files as EdieLemoine  suggested

--- week2/homework/exercises/1-blog-API/server.js
+++ week2/homework/exercises/1-blog-API/server.js
@@ -7,55 +7,83 @@ const app = express();
 
 app.use(express.json());
 
+//setup  posts directory
+const FILES_DIR = __dirname + '/posts-files';
+
+// to handle error
+function isInvalid(req) {
+  if (
+    typeof req.body == 'undefined' ||
+    typeof req.body.title == 'undefined' ||
+    typeof req.body.content == 'undefined'
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 // to create a blog
 app.post('/blogs', (req, res) => {
-  const title = req.body.title;
-  const content = req.body.content;
-  fs.writeFileSync(title, content);
-  res.end('ok');
+  if (isInvalid(req)) {
+    res.status(400);
+    res.send('Invalid Request!! sorry');
+    return;
+  } else {
+    const title = req.body.title;
+    const content = req.body.content;
+    fs.writeFileSync(`${FILES_DIR}/${title}`, content);
+    res.status(200);
+    res.end(`Post ( ${title} ) is created.`);
+  }
 });
 
 // to update and existing blog
 app.put('/posts/:title', (req, res) => {
-  const title = req.params.title;
-  const content = req.body.content;
-  if (fs.existsSync(title)) {
-    fs.writeFileSync(title, content);
-    res.end('ok');
+  if (isInvalid(req)) {
+    res.status(400);
+    res.send('Invalid Request!! sorry');
+    return;
   } else {
-    res.status(400).end('This post does not exist!');
+    const title = req.params.title;
+    const content = req.body.content;
+    if (fs.existsSync(`${FILES_DIR}/${title}`)) {
+      fs.writeFileSync(`${FILES_DIR}/${title}`, content);
+      res.status(200);
+      res.end(`Post ( ${title} ) got updated.`);
+    } else {
+      res.status(400).end(`There is no post with title : ${title}`);
+    }
   }
 });
 
 // to delete a blog
 app.delete('/blogs/:title', (req, res) => {
   const title = req.params.title;
-  if (fs.existsSync(title)) {
-    fs.unlinkSync(title);
-    res.end('ok');
+  if (fs.existsSync(`${FILES_DIR}/${title}`)) {
+    fs.unlinkSync(`${FILES_DIR}/${title}`);
+    res.end(`Post ( ${title} ) got deleted.`);
   } else {
-    res.status(400).end('This blog not found actually!');
+    res.status(400).end(`There is no post with title : ${title}`);
   }
 });
 
 // to read a blog bu title name
 app.get('/blogs/:title', (req, res) => {
   const title = req.params.title;
-  if (fs.existsSync(title)) {
-    const post = fs.readFileSync(title);
+  if (fs.existsSync(`${FILES_DIR}/${title}`)) {
+    const post = fs.readFileSync(`${FILES_DIR}/${title}`);
     res.send(post);
   } else {
-    res.status(400).end('there is no blogs with title :' + title);
+    res.status(400).end(`There is no post with title : ${title}`);
   }
 });
 
 //  to read all post
 app.get('/blogs', (req, res) => {
-  const allFilesTitle = fs.readdirSync(__dirname);
+  const allFilesTitle = fs.readdirSync(FILES_DIR);
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(JSON.stringify(allFilesTitle));
-  /* I tried to just get the files' names that I just posted but I couldn't. 
-  It just bring all files in the folder including the package name and this file name */
 });
 
 // Create server port
